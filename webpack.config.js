566e79690828172025d706716e32927bcfd564f1
EvilSight <demonzgrad@mail.ru>
2021-08-01 23:11:18 +0300

    попытка 6

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/main.js',
-    output: {
-        fileman:'bundle.js',
-        path: path.resolve(__dirname, 'public'),
-    },
-    devtool:'souce-map',
-    devServer: {
-        contentBase: path.resolve(__dirname, 'public'),
-        watchContentBase: true,
-    }
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    watchContentBase: true,
+  }
 };
