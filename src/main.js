954ca4a2f9c8a68f8fcd564d87a29f74463137a1
EvilSight <demonzgrad@mail.ru>
2021-08-02 01:36:37 +0300

    Убрал ошибку 1

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import {createSiteNavTemplate} from './view/site-nav.js';
-import {createUserProfileTemplate} from './view/user-profile.js';
-import {createFilmsSortTemplate} from './view/films-sort.js';
-import {createFilmsListTemplate} from './view/films-list.js';
-import {createFilmsListExtraTemplate} from './view/films-list-extra.js'; //смотри коммент ниже при обьявлении const 38строка
-import {createFilmsCardTemplate} from './view/films-card.js';
-import {createButtonMoreTemplate} from './view/button-more.js';
-import {createStatisticsTemplate} from './view/statistics.js';
-import {createPopupTemplate} from './view/popup.js';
+import { createSiteNavTemplate } from './view/site-nav.js';
+import { createUserProfileTemplate } from './view/user-profile.js';
+import { createFilmsSortTemplate } from './view/films-sort.js';
+import { createFilmsListTemplate } from './view/films-list.js';
+import { createFilmsListExtraTemplate } from './view/films-list-extra.js'; //смотри коммент ниже при обьявлении const 38строка
+import { createFilmsCardTemplate } from './view/films-card.js';
+import { createButtonMoreTemplate } from './view/button-more.js';
+import { createStatisticsTemplate } from './view/statistics.js';
+import { createPopupTemplate } from './view/popup.js';
 
 const TASK_COUNT = 5;
 
@@ -20,25 +20,25 @@ render(siteMainElement, createFilmsSortTemplate(), 'beforeend');
 render(siteMainElement, createFilmsListTemplate(), 'afterbegin');
 
 const siteHeaderElement = siteMainElement.querySelector('.header');
-render(siteMainElement, createUserProfileTemplate(), 'beforeend');
+render(siteHeaderElement, createUserProfileTemplate(), 'beforeend');
 
 const films = document.querySelector('.films');
 for (let i = 0; i < 2; i++) {
-    render(films, createFilmsListExtraTemplate()< 'beforeend');
+    render(films, createFilmsListExtraTemplate() < 'beforeend');
 };
 const filmList = document.querySelector('.films-list');
-render (filmsList, createButtonMoreTemplate(),'beforeend');
+render(filmsList, createButtonMoreTemplate(), 'beforeend');
 
 const filmListContainer = films.querySelector('.films-list__container');
 for (let i = 0; i < TASK_COUNT; i++) {
     render(filmsListContainer, createFilmsCardTemplate(), 'beforeend');
 };
 
-const filmsListExtra = films.querySelectorAll('.films-list--extra'); 
+const filmsListExtra = films.querySelectorAll('.films-list--extra');
 // Потерял мысль по поводу createFilmsListExtraTemplate не понимаю какую функцию или метод нужно использовать далее
 
 const sitePopupElement = document.querySelector('.film-details');
-render (sitePopupElement, createPopupTemplate(), 'beforeend');
+render(sitePopupElement, createPopupTemplate(), 'beforeend');
 
 const siteFooterElement = document.querySelector('.footer__statistics');
 const siteFooterStatisticsElement = siteFooterElement.querySelector('.footer__statistics');
