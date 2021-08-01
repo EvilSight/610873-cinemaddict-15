183669e5d80be982ca003370bbafab885ea320dd
EvilSight <demonzgrad@mail.ru>
2021-08-02 01:51:02 +0300

    9

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: 'src/main.js',
+  entry: './src/main.js',
   output: {
-    filename: 'bundle.js',
+    filename: './bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
   devtool: 'source-map',
