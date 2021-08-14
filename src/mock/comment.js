ab1a8f56a5189aede1d9aa3181d1fc91bda01dfc
Chibisov Dmitriy <demonzgrad@mail.ru>
2021-08-14 16:13:25 +0300

    Добавил Утилитарные функции и Обьекты

---
+++ src/mock/comment.js
@@ -0,0 +1,6 @@
+const emotes = [
+  'smile',
+  'angry',
+  'sleeping',
+  'puke',
+];
---
+++ src/mock/film-card.js
@@ -0,0 +1,62 @@
+const TitlePosterPairs = {
+  'Made for each other': 'made-for-each-other.png',
+  'Popeye meets sinbad': 'popeye-meets-sinbad.png',
+  'Sagebrush trail': 'sagebrush-trail.jpg',
+  'Santa claus conquers the martians': 'santa-claus-conquers-the-martians.jpg',
+  'The dance of life': 'the-dance-of-life.jpg',
+  'The great flamarion': 'the-great-flamarion.jpg',
+  'The man with the golden arm': 'the-man-with-the-golden-arm.jpg',
+};
+
+const descriptions = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget.',
+  'Fusce tristique felis at fermentum pharetra.',
+  'Aliquam id orci ut lectus varius viverra.',
+  'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.',
+  'Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.',
+  'Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.',
+  'Sed sed nisi sed augue convallis suscipit in sed felis.',
+  'Aliquam erat volutpat.',
+  'Nunc fermentum tortor ac porta dapibus.',
+  'In rutrum ac purus sit amet tempus.',
+];
+
+const countries = [
+  'USA',
+  'USSR',
+  'Great Britain',
+  'Germany',
+  'Brazil',
+  'Mexico',
+  'Japan',
+  'Russia',
+];
+
+const genres = [
+  'Comedy',
+  'Horror',
+  'Cartoon',
+  'Family',
+  'Action',
+  'Suspence',
+  'Drama',
+  'Musical',
+  'Sci-fi',
+  'Biography',
+];
+
+const actors = [
+  'Tom Hanks',
+  'Morgan Freeman',
+  'Quentin Tarantino',
+  'Leonardo DiCaprio',
+  'Michael Caine',
+  'Robert De Niro',
+  'Al Pacino',
+  'Gary Oldman',
+  'Matt Damon',
+  'Deni Deniro',
+  'Pol Okenfold',
+];
+
+const ageRatings = ['6+', '12+', '14+', '18+'];
