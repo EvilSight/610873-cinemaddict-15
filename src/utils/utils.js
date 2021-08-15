df790e016ce4b9757037e1155c2d5aa1dfd0808f
Chibisov Dmitriy <demonzgrad@mail.ru>
2021-08-15 23:44:00 +0300

    Наворотил

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -41,3 +41,19 @@ export const getRandomDate = (daySpread = -7, monthSpread = -12, yearSpread = -1
 
   return dayjs().add(dayGap, 'day').add(monthGap, 'month').add(yearGap, 'year').toString();
 };
+
+
+export const RATED_COUNT = 8;
+export const FILM_COUNT_STEP = 5;
+export const FILM_COUNT_EXTRA = 4;
+export const MAX_LENGTH_TEXT = 139;
+export const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+export const getFirstElement = (arr) => arr[0];
+export const getClassName = (variable) => variable ? 'film-card__controls-item film-card__controls-item--active' : 'film-card__controls-item';
+export const getSliceText = (text) => {
+  let newText = text.slice(0, MAX_LENGTH_TEXT);
+  if (text.length > newText.length) {
+    newText += '...';
+  }
+  return newText;
+};
--- view/films-card.js
+++ view/films-card.js
@@ -1,19 +1,28 @@
-export const createFilmsCardTemplate = () => (
-    `<article class="film-card">
-    <h3 class="film-card__title">The Great Flamarion</h3>
-    <p class="film-card__rating">8.9</p>
+import * as dayjs from 'dayjs';
+import { getFirstElement, getClassName, getSliceText } from '../utils/util';
+
+export const createFilmCardTemplate = (params) => {
+  const { title, runtime, genres, poster, description } = params.filmInfo;
+  const rating = params.filmInfo.totalRating;
+  const date = dayjs(params.filmInfo.release.date).format('YYYY');
+  const comments = params.comments.length;
+  const { watchlist, favorite, history } = params.userDetails;
+
+  return `<article class="film-card">
+    <h3 class="film-card__title">${title}</h3>
+    <p class="film-card__rating">${rating}</p>
     <p class="film-card__info">
-      <span class="film-card__year">1945</span>
-      <span class="film-card__duration">1h 18m</span>
-      <span class="film-card__genre">Mystery</span>
+      <span class="film-card__year">${date}</span>
+      <span class="film-card__duration">${runtime}</span>
+      <span class="film-card__genre">${getFirstElement(genres)}</span>
     </p>
-    <img src="./images/posters/the-great-flamarion.jpg" alt="" class="film-card__poster">
-    <p class="film-card__description">The film opens following a murder at a cabaret in Mexico City in 1936, and then presents the events leading up to it in flashback. The Grea…</p>
-    <a class="film-card__comments">12 comments</a>
+    <img src="./images/posters/${poster}" alt="" class="film-card__poster">
+    <p class="film-card__description">${getSliceText(description)}</p>
+    <a class="film-card__comments">${comments} comments</a>
     <div class="film-card__controls">
-      <button class="film-card__controls-item film-card__controls-item--add-to-watchlist" type="button">Add to watchlist</button>
-      <button class="film-card__controls-item film-card__controls-item--mark-as-watched" type="button">Mark as watched</button>
-      <button class="film-card__controls-item film-card__controls-item--favorite" type="button">Mark as favorite</button>
+      <button class="${getClassName(watchlist)} film-card__controls-item--add-to-watchlist" type="button">Add to watchlist</button>
+      <button class="${getClassName(history)} film-card__controls-item--mark-as-watched" type="button">Mark as watched</button>
+      <button class="${getClassName(favorite)} film-card__controls-item--favorite" type="button">Mark as favorite</button>
     </div>
-  </article>`
-);
+  </article>`;
+};
