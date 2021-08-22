379407d12747638647cee1e3e1ea72652d2a97c4
Chibisov Dmitriy <demonzgrad@mail.ru>
2021-08-22 10:51:52 +0300

    поправил имена в сортировке и добавлен верный класс

--- view/films-sort.js
+++ view/films-sort.js
@@ -1,7 +1,7 @@
 export const createSiteSortTemplate = (currentActiveSort) => (
   `<ul class="sort">
-    <li><a href="#" class="sort__button class = "${currentSort === 'default' ? 'active' : ''}"">Sort by default</a></li>
-    <li><a href="#" class="sort__button class = "${currentSort === 'default' ? 'active' : ''}"">Sort by date</a></li>
-    <li><a href="#" class="sort__button class = "${currentSort === 'default' ? 'active' : ''}"sort__button--active">Sort by rating</a></li>
+    <li><a href="#" class="sort__button class = "${currentActiveSort === 'default' ? 'sort__button--active' : ''}"">Sort by default</a></li>
+    <li><a href="#" class="sort__button class = "${currentActiveSort === 'default' ? 'sort__button--active' : ''}"">Sort by date</a></li>
+    <li><a href="#" class="sort__button class = "${currentActiveSort === 'default' ? 'sort__button--active' : ''}"sort__button--active">Sort by rating</a></li>
   </ul>`
 );
