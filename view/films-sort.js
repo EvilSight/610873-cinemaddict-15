aed0f1815f175193f01b83fc23f1cbfb07d512c5
Chibisov Dmitriy <demonzgrad@mail.ru>
2021-08-22 14:10:21 +0300

    исправления в сортировке

--- view/films-sort.js
+++ view/films-sort.js
@@ -1,7 +1,7 @@
 export const createSiteSortTemplate = (currentActiveSort) => (
   `<ul class="sort">
-    <li><a href="#" class="sort__button class = "${currentActiveSort === 'default' ? 'sort__button--active' : ''}"">Sort by default</a></li>
-    <li><a href="#" class="sort__button class = "${currentActiveSort === 'default' ? 'sort__button--active' : ''}"">Sort by date</a></li>
-    <li><a href="#" class="sort__button class = "${currentActiveSort === 'default' ? 'sort__button--active' : ''}"sort__button--active">Sort by rating</a></li>
+    <li><a href="#" class="sort__button ${currentActiveSort === 'default' ? 'sort__button--active' : ''}"">Sort by default</a></li>
+    <li><a href="#" class="sort__button ${currentActiveSort === 'date' ? 'sort__button--active' : ''}"">Sort by date</a></li>
+    <li><a href="#" class="sort__button ${currentActiveSort === 'rating' ? 'sort__button--active' : ''}sort__button--active">Sort by rating</a></li>
   </ul>`
 );
