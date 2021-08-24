fe2801eda9650191eac055284150fc0726f1f3b5
EvilSight <demonzgrad@mail.ru>
2021-08-24 09:25:01 +0300

    comments

--- view/popup.js
+++ view/popup.js
@@ -75,7 +75,7 @@ export const createPopupTemplate = (popup) => {
       </div>
       <div class="film-details__bottom-container">
         <section class="film-details__comments-wrap">
-          <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${countComments}</span></h3>
+          <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${comments.map.join(', ')}</span></h3>
           <ul class="film-details__comments-list">
             <li class="film-details__comment">
               <span class="film-details__comment-emoji">
