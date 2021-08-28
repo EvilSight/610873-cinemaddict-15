c2fe411ad2b5af4565a91f68e0599d21ba393c31
EvilSight <demonzgrad@mail.ru>
2021-08-28 09:35:21 +0300

    поработал над открытием Попапа + генерация комментариев

--- view/popup.js
+++ view/popup.js
@@ -2,16 +2,42 @@
 import * as dayjs from 'dayjs';
 import { getPopupClassName } from '../src/utils/utils.js';
 
+const createCommentPopupTemplate = (comment) => {
+  const { text, authorName, emoji, date } = comment;
+  const dateFormat = dayjs(date).format('DD/MM/YYYY HH:mm');
+
+  return `<li class="film-details__comment">
+    <span class="film-details__comment-emoji">
+      <img src="./images/emoji/${emoji}" width="55" height="55" alt="emoji-smile">
+</span>
+      <div>
+        <p class="film-details__comment-text">${text}</p>
+        <p class="film-details__comment-info">
+          <span class="film-details__comment-author">${authorName}</span>
+          <span class="film-details__comment-day">${dateFormat}</span>
+          <button class="film-details__comment-delete">Delete</button>
+        </p>
+      </div>
+</li>`;
+};
+
 export const createPopupTemplate = (popup) => {
   const date = dayjs(popup.filmInfo.release.date).format('DD MMMM YYYY');
   const { releaseCountry } = popup.filmInfo.release;
   const { title, alternativeTitle, totalRating, poster, ageRating, runtime, description, director, genres } = popup.filmInfo;
   const { writers, actors } = popup.filmInfo;
+  const { watchlist, favorite, history } = popup.userDetails;
+  const comments = popup.comments;
+  const countComments = comments.length;
   const genreTitle = genres.length > 1 ? 'Genres' : 'Genre';
   const getGenres = (genresFilm) => genresFilm.map(
     (genre) =>
       `<span class="film-details__genre">${genre}</span>`,
   ).join('');
+
+  const filterItemsTemplate = comments
+  .map((comment) => createCommentPopupTemplate(comment))
+  .join('');
 
   return `<section class="film-details">
     <form class="film-details__inner" action="" method="get">
@@ -69,66 +95,15 @@ export const createPopupTemplate = (popup) => {
         </div>
         <section class="film-details__controls">
           <button type="button" class="${getPopupClassName(watchlist)} film-details__control-button--watchlist" id="watchlist" name="watchlist">Add to watchlist</button>
-          <button type="button" class="${getPopupClassName(history)} film-details__control-button--active film-details__control-button--watched" id="watched" name="watched">Already watched</button>
+          <button type="button" class="${getPopupClassName(history)} film-details__control-button--watched" id="watched" name="watched">Already watched</button>
           <button type="button" class="${getPopupClassName(favorite)} film-details__control-button--favorite" id="favorite" name="favorite">Add to favorites</button>
         </section>
       </div>
       <div class="film-details__bottom-container">
         <section class="film-details__comments-wrap">
-          <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${comments.map.join(', ')}</span></h3>
+          <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${countComments}</span></h3>
           <ul class="film-details__comments-list">
-            <li class="film-details__comment">
-              <span class="film-details__comment-emoji">
-                <img src="./images/emoji/smile.png" width="55" height="55" alt="emoji-smile">
-              </span>
-              <div>
-                <p class="film-details__comment-text">Interesting setting and a good cast</p>
-                <p class="film-details__comment-info">
-                  <span class="film-details__comment-author">Tim Macoveev</span>
-                  <span class="film-details__comment-day">2019/12/31 23:59</span>
-                  <button class="film-details__comment-delete">Delete</button>
-                </p>
-              </div>
-            </li>
-            <li class="film-details__comment">
-              <span class="film-details__comment-emoji">
-                <img src="./images/emoji/sleeping.png" width="55" height="55" alt="emoji-sleeping">
-              </span>
-              <div>
-                <p class="film-details__comment-text">Booooooooooring</p>
-                <p class="film-details__comment-info">
-                  <span class="film-details__comment-author">John Doe</span>
-                  <span class="film-details__comment-day">2 days ago</span>
-                  <button class="film-details__comment-delete">Delete</button>
-                </p>
-              </div>
-            </li>
-            <li class="film-details__comment">
-              <span class="film-details__comment-emoji">
-                <img src="./images/emoji/puke.png" width="55" height="55" alt="emoji-puke">
-              </span>
-              <div>
-                <p class="film-details__comment-text">Very very old. Meh</p>
-                <p class="film-details__comment-info">
-                  <span class="film-details__comment-author">John Doe</span>
-                  <span class="film-details__comment-day">2 days ago</span>
-                  <button class="film-details__comment-delete">Delete</button>
-                </p>
-              </div>
-            </li>
-            <li class="film-details__comment">
-              <span class="film-details__comment-emoji">
-                <img src="./images/emoji/angry.png" width="55" height="55" alt="emoji-angry">
-              </span>
-              <div>
-                <p class="film-details__comment-text">Almost two hours? Seriously?</p>
-                <p class="film-details__comment-info">
-                  <span class="film-details__comment-author">John Doe</span>
-                  <span class="film-details__comment-day">Today</span>
-                  <button class="film-details__comment-delete">Delete</button>
-                </p>
-              </div>
-            </li>
+            ${filterItemsTemplate}
           </ul>
           <div class="film-details__new-comment">
             <div class="film-details__add-emoji-label"></div>
