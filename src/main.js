399dd82eb1addb79735fdcb433537232a55e2c68
Chibisov Dmitriy <demonzgrad@mail.ru>
2021-09-12 23:19:44 +0300

    Вывел обрабочик из функции (выдавало тайпэррор - при открытии  попапов друг за другом)

--- src/main.js
+++ src/main.js
@@ -20,6 +20,14 @@ const siteMainElement = document.querySelector('.main');
 const siteFooterElement = document.querySelector('.footer');
 const siteFooterStatisticsElement = siteFooterElement.querySelector('.footer__statistics');
 
+const onEscKeyDown = (evt) => {
+  if (evt.key === 'Escape' || evt.key === 'Esc') {
+    document.querySelector('.film-details').remove();
+    document.removeEventListener('keydown', onEscKeyDown);
+    document.querySelector('body').classList.remove('hide-overflow');
+  }
+};
+
 const renderFilmCard = (container, data) => {
   const filmCardComponent = new FilmCardView(data);
   const popupCardComponent = new PopupCardView(data);
@@ -30,6 +38,7 @@ const renderFilmCard = (container, data) => {
     }
     siteFooterElement.appendChild(popupCardComponent.getElement());
     document.querySelector('body').classList.add('hide-overflow');
+    document.addEventListener('keydown', onEscKeyDown);
   };
 
   const filmCard = filmCardComponent.getElement();
@@ -46,13 +55,8 @@ const renderFilmCard = (container, data) => {
     openPopupCard();
   });
 
-  const closePopupCard = () => {
-    document.querySelector('.film-details').remove();
-    document.querySelector('body').classList.remove('hide-overflow');
-  };
-
   popupCardComponent.getElement().querySelector('.film-details__close-btn').addEventListener('click', () => {
-    closePopupCard();
+    onEscKeyDown();
   });
 
   container.appendChild(filmCardComponent.getElement());
